fa60360083f5e176239971a9123f038874b955f2
Martin Maga <Martin.Maga@centrum.sk>
2013-05-19 21:09:00 +0200

    Skuska Unit Testu

--- UnitTest.py
+++ UnitTest.py
@@ -2,17 +2,4 @@
 
 import unittest
 
-class SortedDict(dict):
-    """
-    Dictionary that maintains keys in sorted order (by time of definition).
-    """
-    def __init__(self, *args, **kwargs):
-        super(SortedDict, self).__init__(*args, **kwargs)
-        self._key_order = []
-
-    def __setitem__(self, key, value):
-        if key not in self:
-            self._key_order.append(key)
-        super(SortedDict, self).__setitem__(key, value)
-
-    def keys(self):
+unittest.main()
