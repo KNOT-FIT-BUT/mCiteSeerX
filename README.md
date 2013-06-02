7f6fd0bcea546cde4d60dfd504b93bd1873cff3d
Martin Maga <Martin.Maga@centrum.sk>
2013-06-02 12:36:02 +0200

    Oprava

---
+++ README.md
@@ -0,0 +1,10 @@
+mCiteSeerX
+Python API module for citeseerx.ist.psu.edu
+
+Try it
+
+In ./bin/example/ you can find example app using citeseerx.py module.
+
+More informations:
+
+https://merlin.fit.vutbr.cz/nlp-wiki/index.php/Rrs_cite_comparsion
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='Citeseerx',
+    version='0.1.0',
+    author='Martin Maga',
+    author_email='xmagam00@stud.fit.vutbr.cz',
+    packages=['citeseerx','citeseerx.test'],
+    scripts=['bin/citeseerx.py'],
+    url='https://github.com/KNOT-GIT/mCiteSeerX',
+    license='LICENSE.txt',
+    description='Python API module for citeseerx.ist.psu.edu',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "Beautiful Soup 4.0.1",
+    ],
+)
