f20749efd1163107c886b705ed258204ab7e84bc
Casey <casey@vm-ubuntu-12.(none)>
2013-06-02 21:47:12 +0200

    Update of setup.py and rename __init.py in citeseerx directory

--- setup.py
+++ setup.py
@@ -5,13 +5,12 @@ setup(
     version='0.1.0',
     author='Martin Maga',
     author_email='xmagam00@stud.fit.vutbr.cz',
-    packages=['dist'],
-    scripts=['citeseerx/citeseerx.py'],
+    packages=['citeseerx'],
     url='https://github.com/KNOT-GIT/mCiteSeerX',
     license='LICENSE.txt',
     description='Python API module for citeseerx.ist.psu.edu',
     long_description=open('README.md').read(),
     install_requires=[
-        "Beautiful Soup 4.0.1",
+        "beautifulsoup4 >= 4.0.1",
     ],
 )
